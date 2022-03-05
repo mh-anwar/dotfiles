f8ab81f966e50930412bd8f510042f1002c6e9ce
mh-anwar <mohammad.anwar2939@gmail.com>
2022-03-05 18:41:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#ubuntu-configs
+
+##Install:
+- i3
+- dunst
+- polybar
+- pamixer
+
+##Additional Installs: 
+- brave
+- firefox
+- joplin
+- keepassxc
+- kdeconnect
+- signal
+- superproductivity
+- xampp
+- zsh (ohmyzsh)
+- zoom
---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar the_one_bar 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
