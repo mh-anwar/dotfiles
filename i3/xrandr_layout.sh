ad3b2f1cb57a8647f4974e8c74ca25744fbf3c73
mh-anwar <mohammad.anwar2939@gmail.com>
2024-07-17 04:52:26 -0400

    Update xrandr layout script

--- i3/xrandr_layout.sh
+++ i3/xrandr_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DisplayPort-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI-A-0 --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off
