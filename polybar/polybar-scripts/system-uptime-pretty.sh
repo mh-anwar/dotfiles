efce9e217e96a9748c3a4ba9c6191336b40a9937
mh-anwar <mohammad.anwar2939@gmail.com>
2025-02-25 00:13:40 -0500

    add: new uptime display script

--- polybar/polybar-scripts/system-uptime-pretty.sh
+++ polybar/polybar-scripts/system-uptime-pretty.sh
@@ -2,5 +2,35 @@
 
 #!/bin/sh
 
-uptime --pretty | sed -E 's/up //; s/([0-9]+) years?/, &y/; s/([0-9]+) weeks?/, &w/; s/([0-9]+) days?/, &d/; s/([0-9]+) hours?/, &h/; s/, [0-9]+ minutes?//g; s/,/ /g; s/ hours?//g; s/ days?//g; s/^[ ,]+//; s/[ ,]+$//; s/[ ,]+/ /g'
+# uptime --pretty | sed -E 's/up //; s/([0-9]+) years?/, &y/; s/([0-9]+) weeks?/, &w/; s/([0-9]+) days?/, &d/; s/([0-9]+) hours?/, &h/; s/, [0-9]+ minutes?//g; s/,/ /g; s/ hours?//g; s/ days?//g; s/^[ ,]+//; s/[ ,]+$//; s/[ ,]+/ /g'
 #| sed 's/\ minutes\?/m/'
+
+
+# Get the uptime information
+uptime_output=$(uptime -p)
+
+# Parse the uptime into simplified format
+simplified_uptime=$(echo "$uptime_output" | awk '
+{
+    # Remove "up" from the output
+    sub("up ", "")
+
+    # Replace "weeks", "days", "hours", "minutes" with w, d, h, m
+    gsub("weeks?", "w")
+    gsub("days?", "d")
+    gsub("hours?", "h")
+    gsub("minutes?", "m")
+
+    # Split on commas and trim whitespace
+    n = split($0, arr, ",")
+    result = ""
+    for (i = 1; i <= n; i++) {
+        gsub(/^ +| +$/, "", arr[i])
+        result = result (result ? " " : "") arr[i]
+    }
+
+    print result
+}')
+
+# Output the simplified uptime
+echo "$simplified_uptime"
